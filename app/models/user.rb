80d32d08580efef94683333e4b1e34603e9353af
klopster <klop@seznam.cz>
2017-02-19 23:02:24 +0100

    changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
 	validates :name, presence: true , length: {maximum: 50}
 	
-	validates :email, presence: true , length: {maximum: 256}
+	#regex exprssion made by Rubular.com
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true , length: {maximum: 256} , format: {with: VALID_EMAIL_REGEX}
 	
 
 end
